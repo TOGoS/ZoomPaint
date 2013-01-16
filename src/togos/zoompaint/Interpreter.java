ac1a98f74e78b94dc9de5520c4dac694ccb302a0
TOGoS <togos00@gmail.com>
2013-01-16 09:50:07 -0600

    Can now generate images and save them.

--- src/togos/zoompaint/Interpreter.java
+++ src/togos/zoompaint/Interpreter.java
@@ -1,13 +1,49 @@
 package togos.zoompaint;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.WeakHashMap;
+
+import javax.imageio.ImageIO;
 
 public class Interpreter
 {
+	static class NodeImageCache {
+		WeakHashMap<Node,BufferedImage> images = new WeakHashMap();
+		
+		protected void getPixels( Node n, int[] pix, int off, int scan, int depth ) {
+			if( depth == 0 ) {
+				pix[off] = n.color;
+			} else {
+				int size = (1 << depth);
+				int subSize = size>>1;
+				getPixels(n.n0, pix, off,                      scan, depth-1);
+				getPixels(n.n1, pix, off+subSize,              scan, depth-1);
+				getPixels(n.n2, pix, off+scan*subSize,         scan, depth-1);
+				getPixels(n.n3, pix, off+scan*subSize+subSize, scan, depth-1);
+			}
+		}
+		
+		public BufferedImage getNodeImage( Node n ) {
+			BufferedImage img = images.get(n);
+			if( img == null ) {
+				int[] pix = new int[64*64];
+				getPixels( n, pix, 0, 64, 6 );
+				img = new BufferedImage( 64, 64, BufferedImage.TYPE_INT_ARGB );
+				img.setRGB(0,  0, 64, 64, pix, 0, 64);
+				images.put(n, img);
+			}
+			return img;
+		}
+	}
+	
 	public static void main( String[] args ) throws Exception {
+		NodeImageCache c = new NodeImageCache();
+		
 		ArrayList stack = new ArrayList();
 		HashMap<Object,Object> saved = new HashMap<Object,Object>();
 		BufferedReader lineReader = new BufferedReader(new InputStreamReader(System.in));
@@ -17,11 +53,11 @@ public class Interpreter
 			for( String p : parts ) {
 				if( saved.containsKey(p) ) {
 					stack.add( saved.get(p) );
-				} else if( "t".equals(p) ) {
-					stack.add(Boolean.TRUE);
-				} else if( "f".equals(p) ) {
-					stack.add(Boolean.FALSE);
-				} else if( "node".equals(p) ) {
+				} else if( "empty".equals(p) ) {
+					stack.add(Node.EMPTY);
+				} else if( "self".equals(p) ) {
+					stack.add(Node.SELF);
+				} else if( "make-node".equals(p) ) {
 					int argsAt = stack.size()-4;
 					stack.add(new Node(
 						stack.remove(argsAt),
@@ -31,10 +67,18 @@ public class Interpreter
 					));
 				} else if( "dup".equals(p) ) {
 					stack.add(stack.get(stack.size()-1));
-				} else if( "save".equals(p) ) {
+				} else if( "set".equals(p) ) {
 					saved.put( stack.remove(stack.size()-1), stack.remove(stack.size()-1) );
+				} else if( "to-image".equals(p) ) {
+					stack.add(c.getNodeImage( (Node)stack.remove(stack.size()-1) ));
+				} else if( "save-image".equals(p) ) {
+					String name = (String)stack.remove(stack.size()-1);
+					BufferedImage img = (BufferedImage)stack.remove(stack.size()-1);
+					ImageIO.write( img, "png", new File(name) );
 				} else if( p.startsWith("'") ) {
 					stack.add( p.substring(1) );
+				} else {
+					throw new RuntimeException("Unrecognised word: "+p);
 				}
 			}
 		}
--- src/togos/zoompaint/Node.java
+++ src/togos/zoompaint/Node.java
@@ -2,10 +2,12 @@ package togos.zoompaint;
 
 public final class Node
 {
+	public static final Object SELF = new Object();
+	
 	public final Node n0, n1, n2, n3;
 	public final int color;
 	
-	public static final Node EMPTY = new Node(true,true,true,true);
+	public static final Node EMPTY = new Node(SELF,SELF,SELF,SELF);
 	
 	protected static boolean isAtom( Node n ) {
 		return
@@ -18,10 +20,10 @@ public final class Node
 	protected static int calculateColor( Node n ) {
 		if( isAtom(n) ) {
 			return Color.color(
-				n.n0 == EMPTY ? 0xFF : 0,
-				n.n1 == EMPTY ? 0xFF : 0,
-				n.n2 == EMPTY ? 0xFF : 0,
-				n.n3 == EMPTY ? 0xFF : 0
+				n.n0 == n ? 0xFF : 0,
+				n.n1 == n ? 0xFF : 0,
+				n.n2 == n ? 0xFF : 0,
+				n.n3 == n ? 0xFF : 0
 			);
 		} else {
 			return Color.average( n.n0.color, n.n1.color, n.n2.color, n.n3.color );
@@ -29,8 +31,7 @@ public final class Node
 	}
 	
 	protected Node parseSubNode( Object o ) {
-		if( o == Boolean.TRUE ) return this;
-		if( o == Boolean.FALSE || o == null ) return EMPTY;
+		if( o == SELF ) return this;
 		if( o instanceof Node ) return (Node)o;
 		throw new RuntimeException("Invalid node argument: "+o.getClass());
 	}
@@ -41,9 +42,5 @@ public final class Node
 		this.n2 = parseSubNode(n2);
 		this.n3 = parseSubNode(n3);
 		this.color = calculateColor(this);
-	}
-	
-	public Node( boolean n0, boolean n1, boolean n2, boolean n3 ) {
-		this( Boolean.valueOf(n0), Boolean.valueOf(n1), Boolean.valueOf(n2), Boolean.valueOf(n3) );
 	}
 }
